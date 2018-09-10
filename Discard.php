54b2eeff6b5cab0ae4d38fbfd78479dc650ff3f3
AnthorNet <anthor.net@gmail.com>
2018-09-10 11:38:55 +0200

    Add discarded event list

---
+++ Discard.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Elite: Dangerous Star Map
+ *
+ * @link        https://www.edsm.net/
+ * @copyright   Copyright (c) 2015-2017 EDSM.
+ */
+ 
+namespace   Journal;
+
+class Discard
+{
+    // Events that we discard
+    static public $events = [
+        // EDMC / EDD / EDDI
+        //'StartUp', // Give first system response to EDMC
+        'ShutDown',
+        'EDDCommodityPrices',
+        'ModuleArrived',
+        'ShipArrived',
+        'Coriolis',
+        'EDShipyard',
+        
+        // Extra files (Taking them from EDDN)
+        'Market',
+        'Shipyard',
+        'Outfitting',
+        'ModuleInfo',
+        'Status',
+        
+        // Load events
+        'Fileheader',
+        'Commander',
+        'NewCommander',
+        'ClearSavedGame',
+        'Music',
+        'Continued',
+        'Passengers',
+        
+        // Docking events
+        'DockingCancelled',
+        'DockingDenied',
+        'DockingGranted',
+        'DockingRequested',
+        'DockingTimeout',
+        
+        // Fly events
+        'StartJump',
+        'Touchdown',
+        'Liftoff',
+        'ApproachSettlement',
+        'NavBeaconScan',
+        'SupercruiseEntry',
+        'SupercruiseExit',
+        
+        // We might reconsider this, and see if we can do something about crime report?
+        'UnderAttack',
+        'ShipTargeted',
+        'Scanned',
+        'DataScanned',
+        'DatalinkScan',
+        
+        // Engineer
+        'EngineerApply',
+        'EngineerLegacyConvert',
+        
+        // Reward (See RedeemVoucher for credits)
+        'FactionKillBond',
+        'Bounty',
+        'CapShipBond',
+        'DatalinkVoucher',
+        
+        // Ship events
+        'SystemsShutdown',
+        'EscapeInterdiction',
+        'HeatDamage',
+        'HeatWarning',
+        'HullDamage',
+        'ShieldState',
+        'FuelScoop',
+        'LaunchDrone',
+        'AfmuRepairs',
+        'CockpitBreached',
+        
+        'ApproachBody',
+        'LeaveBody',
+        'DiscoveryScan',
+        'MaterialDiscovered',
+        'Screenshot',
+        
+        // NPC Crew
+        'CrewAssign',
+        'CrewFire',
+        'NpcCrewRank',
+        
+        // Shipyard / Outfitting
+        'ShipyardNew',
+        'StoredModules',
+        'MassModuleStore',
+        'ModuleStore',
+        'ModuleSwap',
+        
+        // Powerplay
+        'PowerplayVote',
+        'PowerplayVoucher',
+        
+        'ChangeCrewRole',
+        'CrewLaunchFighter',
+        'CrewMemberJoins',
+        'CrewMemberQuits',
+        'CrewMemberRoleChange',
+        'KickCrewMember',
+        'EndCrewSession', // ??
+        
+        'LaunchFighter',
+        'DockFighter',
+        'FighterDestroyed',
+        'FighterRebuilt',
+        'VehicleSwitch',
+        'LaunchSRV',
+        'DockSRV',
+        'SRVDestroyed',
+        
+        'JetConeBoost',
+        'JetConeDamage',
+        
+        'RebootRepair',
+        'RepairDrone',
+        
+        // Wings
+        'WingAdd',
+        'WingInvite',
+        'WingJoin',
+        'WingLeave',
+        
+        // Chat
+        'ReceiveText',
+        'SendText',
+        
+        // End of game
+        'Shutdown',
+    ];
+}
